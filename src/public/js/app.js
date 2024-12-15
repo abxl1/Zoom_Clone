a845e45a4612e0e6e3386218dcb550359f70f009
abxl1 <etho_818@naver.com>
2024-12-15 21:01:44 +0900

    [FIX] app.js 닉네임 저장 시 input 비우기로 수정

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,7 +5,12 @@ const socket = new WebSocket(`ws://${window.location.host}`); // 사용자의 lo
 
 function makeMsg(type, payload) {
     const msg = { type, payload };
-    return JSON.stringify(msg); // Object -> string 변환
+    return JSON.stringify(msg); 
+    // stringify() : Object -> string 변환
+    // parse() : string -> Object 변환
+    // BE에서는 js의 object를 알지 못함 -> 문자열로 변환 후 전달 필요
+    // 웹소켓이 브라우저에 있는 API이기 때문에 API에 결정을 맡기면 안 됨
+    // BE에서 string을 전달받고 FE에서 다시 object를 string으로 변환하여 UI 출력 
 }
 
 socket.addEventListener("open", () => {
@@ -33,6 +38,7 @@ function handleNickSubmit(event) {
     event.preventDefault();
     const input = nickForm.querySelector("input");
     socket.send(makeMsg("nickname", input.value));
+    input.value = "";
 }
 
 msgForm.addEventListener("submit", handleSubmit);
