13da50ac2051b0110a612b72d61f93e3f5d7d9f0
abxl1 <etho_818@naver.com>
2024-12-15 16:05:50 +0900

    [FEAT] app.js 서버 오픈, 메시지, 클로즈에 해당하는 이벤트 리스너 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = new WebSocket(`ws://${window.location.host}`); // 사용자의 localhost를 환경변수로 처리
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server ✅");
+})
+
+socket.addEventListener("message", (message) => {
+    console.log("New message :", message.data);
+})
+
+socket.addEventListener("close", () => {
+    console.log("DisConnected to Server ❌");
+})
+
+setTimeout(() => {
+    socket.send("hello from the browser!");
+}, 10000) // 10초 후 메시지 출력
