2a5a213d5793b2db76cce26de965198553e6fd9e
abxl1 <etho_818@naver.com>
2024-12-15 16:05:08 +0900

    [FEAT / FIX] server.js 프론트엔드 메시지 주고받기 구현, 불필요한 함수 제거

--- src/server.js
+++ src/server.js
@@ -7,17 +7,21 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.render("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.render("home"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server}); // http 서버 위에 ws 서버 사용
 
-function handleConnection(socket) {
-    console.log(socket);
-}
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser ✅");
+    socket.on("close", () => console.log("DisConnected from the Browser ❌"));
+    socket.on("message", (message) => {
+        console.log(message.toString());
+    });
+    socket.send("hello!");
+});
 
-wss.on("connection", handleConnection);
 server.listen(3000, handleListen); // 두 개의 프로토콜이 같은 포트를 사용함
