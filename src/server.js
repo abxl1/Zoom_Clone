9ba304f765bf466a549ceffac691d7ace4bb920b
abxl1 <etho_818@naver.com>
2024-12-15 19:46:24 +0900

    [FIX] server.js 여러 브라우저에서 출력하도록 수정

--- src/server.js
+++ src/server.js
@@ -15,13 +15,18 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server}); // http 서버 위에 ws 서버 사용
 
+const sockets = []; // fake database
+
 wss.on("connection", (socket) => {
+    sockets.push(socket); // 모든 소켓을 소켓 객체에 넣기
     console.log("Connected to Browser ✅");
     socket.on("close", () => console.log("DisConnected from the Browser ❌"));
     socket.on("message", (message) => {
-        console.log(message.toString());
+
+        // 각 브라우저를 aSocket으로 취급, 모든 브라우저에 메시지 전송
+        sockets.forEach(aSocket => aSocket.send(message.toString()));
+        socket.send(message.toString());
     });
-    socket.send("hello!");
 });
 
 server.listen(3000, handleListen); // 두 개의 프로토콜이 같은 포트를 사용함
